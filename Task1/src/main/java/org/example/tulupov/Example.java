ec2fd410e3ec250a89bd89fb3f3a123a8dc1170a
mikhail <kd.maikl-201@yandex.ru>
2023-01-12 09:17:03 +0700

    First commit

---
+++ Task1/src/main/java/org/example/tulupov/Example.java
@@ -0,0 +1,8 @@
+package org.example.tulupov;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
