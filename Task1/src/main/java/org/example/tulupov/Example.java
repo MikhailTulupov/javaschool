61d0c13e855f69148a014478ef17a7bb0aa7601d
mikhail <kd.maikl-201@yandex.ru>
2023-01-12 09:19:21 +0700

    commit

--- Task1/src/main/java/org/example/tulupov/Example.java
+++
@@ -1,8 +0,0 @@
-package org.example.tulupov;
-
-public class Example {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-    }
-}
