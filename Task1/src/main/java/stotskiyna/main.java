2472c791f033586449a8be40ebe96e7d74d98932
S1nt0z <s1ntozoffical@gmail.com>
2023-01-12 19:57:08 +0700

    my final message goodbye

--- Task1/src/main/java/stotskiyna/main.java
+++ Task1/src/main/java/stotskiyna/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         String name = "Stotskiy Nikita";
         System.out.println(name);
+        System.out.printf("Goodbye");
     }
 }
