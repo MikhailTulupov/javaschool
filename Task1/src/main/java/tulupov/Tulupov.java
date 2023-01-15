61d19ed1406944065e22678c065625ed571f7391
S1nt0z <s1ntozoffical@gmail.com>
2023-01-15 10:41:26 +0700

    My final message (Xexex) Stotskiy - my first commit

--- Task1/src/main/java/tulupov/Tulupov.java
+++
@@ -1,7 +0,0 @@
-package tulupov;
-
-public class Tulupov {
-    public static void main(String[] args) {
-        System.out.println("Tulupov");
-    }
-}
--- Task1/src/main/java/tuzov/Tuzov.java
+++
@@ -1,7 +0,0 @@
-package tuzov;
-
-public class Tuzov {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
