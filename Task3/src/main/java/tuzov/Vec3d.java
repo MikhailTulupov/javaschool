89fb40047b5c1703c8211247cc37264a99c29fe5
stotskiyna <LenovoNikita2017@yandex.ru>
2023-01-24 13:00:59 +0700

    Tuzov - Task 3 - method names refactored (snake_case -> camelCase)

--- Task3/src/main/java/tuzov/Vec3d.java
+++ Task3/src/main/java/tuzov/Vec3d.java
@@ -15,29 +15,29 @@ public class Vec3d {
     }
 
 
-    public double get_length() {
+    public double getLength() {
         return Math.sqrt(Math.pow(this.x, 2) +
                 Math.pow(this.y, 2) +
                 Math.pow(this.z, 2));
     }
 
 
-    public static int scalar_product(Vec3d vec1, Vec3d vec2) {
+    public static int scalarProduct(Vec3d vec1, Vec3d vec2) {
         return vec1.x * vec2.x +
                 vec1.y * vec2.y +
                 vec1.z * vec2.z;
     }
 
 
-    public static Vec3d vector_product(Vec3d vec1, Vec3d vec2) {
+    public static Vec3d vectorProduct(Vec3d vec1, Vec3d vec2) {
         return new Vec3d(vec1.y * vec2.z -  vec1.z * vec2.y,
                 vec1.z * vec2.x - vec1.x * vec2.z,
                 vec1.x * vec2.y - vec1.y * vec2.x);
     }
 
 
-    public static double angle_between(Vec3d vec1, Vec3d vec2) {
-        return Vec3d.scalar_product(vec1, vec2) / (vec1.get_length() * vec2.get_length());
+    public static double angleBetween(Vec3d vec1, Vec3d vec2) {
+        return Vec3d.scalarProduct(vec1, vec2) / (vec1.getLength() * vec2.getLength());
     }
 
 
