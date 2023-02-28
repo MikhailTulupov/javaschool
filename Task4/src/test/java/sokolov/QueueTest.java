a3a4e19167dc7a23c4dfdf8e33964f3e14a7e4be
lilBatman <domestos_15@mail.ru>
2023-02-28 11:00:04 +0700

    Sokolov - Task 4

---
+++ Task4/src/test/java/sokolov/QueueTest.java
@@ -0,0 +1,52 @@
+package sokolov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class QueueTest {
+    Queue queue;
+
+    @BeforeEach
+    public void init() {
+        queue = new Queue(3);
+    }
+
+    @Test
+    public void testEnqueueFull() {
+        queue.enqueue(1);
+        queue.enqueue(3);
+        queue.enqueue(5);
+        System.out.println(queue.isFull());
+        queue.dequeue();
+        System.out.println(queue.str());
+        queue.dequeue();
+        System.out.println(queue.str());
+        queue.dequeue();
+        System.out.println(queue.str());
+        System.out.println(queue.isEmpty());
+    }
+
+    @Test
+    public void testDequeueEmpty() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> queue.dequeue());
+    }
+
+
+    @Test
+    public void testTopEqualsDequeue() {
+        queue.enqueue(2);
+        assertEquals(queue.getFront(), queue.dequeue());
+    }
+
+    @Test
+    public void testDequeueOrder() {
+        int i = 4;
+        queue.enqueue(i);
+        queue.enqueue(6);
+        queue.enqueue(2);
+        assertEquals(queue.dequeue(), i);
+    }
+}
